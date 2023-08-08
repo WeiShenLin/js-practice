4b24233ee80f578da4773053f82174bc8d6c0952
EC2 Default User <ec2-user@ip-172-31-6-9.ap-southeast-2.compute.internal>
2023-08-08 18:11:15 +0000

    use bootstrap

--- NodeProject/app.js
+++ NodeProject/app.js
@@ -1,2 +1,21 @@
-var aloha = 'aloha var';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+
+app.get('/',function(req,res){
+    res.send('aloha world!');
+});
+
+app.get('/routing',function(req,res){
+    res.send('aloha routing!');
+});
+
+app.listen(port,function(err){
+    console.log('The server is running on port: ' + port);    
+});
